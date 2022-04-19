a6407e136f3764dcd89585757cabb45bf4c1ef4e
yoonification <yoonsung68@gmail.com>
2022-04-19 13:24:30 -0500

    HW5,HW6

---
+++ HW5.X/main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: bluew
+ *
+ * Created on April 19, 2022, 2:55 AM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
