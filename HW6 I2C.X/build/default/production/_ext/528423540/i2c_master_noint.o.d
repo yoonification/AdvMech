2b2ee170f1adf6cb5cc9540b076b31ef1eb424a5
yoonification <yoonsung68@gmail.com>
2022-04-27 04:06:46 -0500

    deleted

--- HW6 I2C.X/build/default/production/_ext/528423540/i2c_master_noint.o.d
+++
@@ -1,5 +0,0 @@
-build/default/production/_ext/528423540/i2c_master_noint.o: \
- ../../ME433_2022/i2c/i2c_master_noint.c \
- ../../ME433_2022/i2c/i2c_master_noint.h
-
-../../ME433_2022/i2c/i2c_master_noint.h:
